97382ff67e29dce9cd23ecee56c8e0dda4844d88
Artur B <bachtartur02g@mail.com>
2024-03-12 22:50:31 +0100

    Detecting PNG files

---
+++ EMedia 1/PngReader.cs
@@ -0,0 +1,22 @@
+﻿namespace EMedia_1;
+
+public class PngReader(Stream stream)
+{
+    public static readonly byte[] PngSignature = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+    public static int SignatureLength => PngSignature.Length;
+
+    public async Task ReadAsync()
+    {
+        var isPng = await CheckSignatureAsync();
+        Console.WriteLine(isPng ? "File is png" : "File is not png");
+    }
+
+    private async Task<bool> CheckSignatureAsync()
+    {
+        var buffer = new byte[SignatureLength];
+        var bytesRead = await stream.ReadAsync(buffer.AsMemory(0, SignatureLength));
+
+        return buffer.Take(bytesRead)
+            .SequenceEqual(PngSignature);
+    }
+}
---
+++ EMedia 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using EMedia_1;
+
+Console.WriteLine("Enter filename to open");
+
+var filename = Console.ReadLine();
+
+if (!File.Exists(filename))
+{
+    PrintError($"File {filename} does not exist");
+    return;
+}
+
+await using var stream = File.Open(filename, FileMode.Open);
+var reader = new PngReader(stream);
+
+await reader.ReadAsync();
+
+void PrintError(string error)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Error: {error}");
+    Console.ResetColor();
+}
